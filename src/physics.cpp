c6104afdb19db710ac163a645b0f8666b2ad30e3
bradleymorton <bradley.m.morton@gmail.com>
2019-04-10 15:32:00 -0800

    added basic physics class, it does not compile, and I don't understand the error.

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,1 +1,6 @@
 //File for physics related code definitions for the new and improved SegFault
+#include "physics.hpp"
+
+Physics::Physics(double xPosition, double yPosition, double xVelocity, double yVelocity)
+ : _xPosition{xPosition}, _yPosition{yPosition}, _xVelocity{xVelocity}, _yVelocity{yVelocity}, _pi{3.1415926535897932384626}, _frameRate{60}
+{}
--- src/physics.hpp
+++ src/physics.hpp
@@ -1,1 +1,37 @@
 //File for physics related code prototypes for the new and improved SegFault
+
+#ifndef PHYSICS_HPP
+#define PHYSICS_HPP
+
+
+
+
+class Physics
+{
+private:
+double _xVelocity, _yVelocity, _xPosition, _yPosition;
+const double _pi, _frameRate;
+public: 
+
+
+Physics::Physics(double xPosition, double yPosition, double xVelocity, double yVelocity);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
