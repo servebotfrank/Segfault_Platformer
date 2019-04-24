9c74b18ef3385ec9f5b2d7e9d304f2aa92184e28
Erin <eetolman@hotmail.com>
2019-04-24 12:43:19 -0800

    merge conflict

---
+++ cmake-build-debug/CMakeFiles/platformer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/platformer.dir/src/main.cpp.o"
+  "../bin/platformer.pdb"
+  "../bin/platformer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/platformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/platformer_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/platformer_tests.dir/tests/tests_main.cpp.o"
+  "CMakeFiles/platformer_tests.dir/tests/tests.cpp.o"
+  "../bin/platformer_tests.pdb"
+  "../bin/platformer_tests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/platformer_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
